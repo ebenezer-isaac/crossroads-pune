0f08b0f0c4a5d5c1a447227e9ce20bcf033a6ccb
Ebenezer Isaac <mail@ebenezer-isaac.com>
2024-07-05 17:00:21 +0530

    teaser update

--- assets/js/video.js
+++ assets/js/video.js
@@ -1,27 +1,28 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var videos = document.querySelectorAll("video");
+  // Select all videos inside the gallery div, excluding the videoPlayer
+  var videos = document.querySelectorAll("#gallery video");
 
-    function checkVideo() {
-      videos.forEach(function(video) {
-        var rect = video.getBoundingClientRect();
-        var viewportHeight = window.innerHeight || document.documentElement.clientHeight;
+  function checkVideo() {
+    videos.forEach(function(video) {
+      var rect = video.getBoundingClientRect();
+      var viewportHeight = window.innerHeight || document.documentElement.clientHeight;
 
-        // Check if the video is in the viewport
-        if (rect.top >= 0 && rect.bottom <= viewportHeight) {
-          if (video.paused) {
-            video.play();
-          }
-          video.loop = true;
-        } else {
-          video.pause();
+      // Check if the video is in the viewport
+      if (rect.top >= 0 && rect.bottom <= viewportHeight) {
+        if (video.paused) {
+          video.play();
         }
-      });
-    }
+        video.loop = true;
+      } else {
+        video.pause();
+      }
+    });
+  }
 
-    // Initial check when the page loads
-    checkVideo();
+  // Initial check when the page loads
+  checkVideo();
 
-    // Check again on scroll or resize
-    window.addEventListener("scroll", checkVideo);
-    window.addEventListener("resize", checkVideo);
-  });
+  // Check again on scroll or resize
+  window.addEventListener("scroll", checkVideo);
+  window.addEventListener("resize", checkVideo);
+});
